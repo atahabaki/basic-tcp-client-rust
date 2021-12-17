7f7687656f2cc2fb70e57baf52b1c2fcdeef3f68
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2021-12-17 03:23:04 +0300

    POST method fixed...

--- src/app_argument.rs
+++ src/app_argument.rs
@@ -19,13 +19,26 @@ impl Default for AppArgument {
 impl ToString for AppArgument {
   fn to_string(&self) -> String {
     let l1 = format!(
-      "{} {} HTTP/1.1",
+      "{} /{} HTTP/1.1",
       self.method.to_string(),
-      self.address.path.as_ref().unwrap_or(&String::from("/"))
+      self.address.path.as_ref().unwrap_or(&String::new())
     );
-    let l2 = format!("Host: {}", self.address.host);
+    let l2 = format!(
+      "Host: {}:{}",
+      self.address.host,
+      self.address.port.as_ref().unwrap_or(&String::from("80"))
+    );
     let l3 = format!("{}", self.body.as_ref().unwrap_or(&String::new()));
-    let l = format!("{}\n{}\n{}\n\n", l1, l2, l3);
+    let l = if self.method.to_string() == Method::GET.to_string() {
+      format!("{}\n{}\r\n\r\n{}\r\n", l1, l2, l3)
+    } else {
+      let l5 = format!(
+        "Content-Length: {}",
+        self.body.as_ref().unwrap_or(&String::new()).len() + 1
+      );
+      let l6 = "Content-Type: application/json";
+      format!("{}\r\n{}\r\n{}\r\n{}\r\n\r\n{}\r\n", l1, l2, l6, l5, l3)
+    };
     dbg!("{:#?}", &l);
     l
   }
